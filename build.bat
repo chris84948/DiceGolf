1501bfb57394fd390cc9d679baaa04350dc339d2
Unknown <chris84948@gmail.com>
2018-11-30 20:14:06 -0600

    Making sure libraries are properly added pt1

--- build.bat
+++ build.bat
@@ -1,9 +1,15 @@
 rmdir build /s /q
 del /f DiceGolf.love
+del /f "I Dice Big Putts.zip"
 
-7z.exe a DiceGolf.zip * -r -x!*.bat -x!*.piskel -x!*.svg
+7z.exe a DiceGolf.zip * -r -x!*.bat -x!*.piskel -x!*.svg -x!*.gif
 ren DiceGolf.zip DiceGolf.love
 
 CALL "../build.bat" DiceGolf.love
 
 del /f DiceGolf.love
+
+ren Build "I Dice Big Putts"
+7z.exe a "I Dice Big Putts.zip" "I Dice Big Putts\*" -r
+
+rmdir "I Dice Big Putts" /s /q
